1d80c9c2f0e5efd493eff17621ed6642bd4e146b
Dmitry Kovalev <dakovalev1@mail.ru>
2019-04-06 12:12:00 +0300

    test without menu

--- js/scroll.js
+++ js/scroll.js
@@ -3,11 +3,23 @@ target = target.replace('#', '');
 window.location.hash = "";
 
 
+/*function scrollToAnchor(target){
+    target=(typeof target==='undefined'||typeof target==='object')?window.location.hash:target;
+    target=target.replace(/:/g,'\\:');
+    if($(target).length){
+        $('body').addClass('scrolling');
+        $('html, body').animate({
+            scrollTop:$(target).offset().top-navbar_offset},
+            600,
+            function(){$('body').removeClass('scrolling');}
+        );
+    }
+}*/
 
 
 
 $(document).ready(function(){
-    if ( /webkit.*mobile/i.test(navigator.userAgent)) {
+    /*if ( /webkit.*mobile/i.test(navigator.userAgent)) {
         (function($) {
             $.fn.offsetOld = $.fn.offset;
             $.fn.offset = function() {
@@ -17,7 +29,7 @@ $(document).ready(function(){
               return result;
             };
         })(jQuery);
-    }
+    }*/
     
 
     function scroll(elem){
@@ -29,11 +41,12 @@ $(document).ready(function(){
             } else {
                 offset = 50;
             }
+            offset = 0;
 
             //offset -=  $("div.menu-container").offset().top;
 
-            $('html, body').stop(true);
-            $('html, body').animate({
+            $('body').stop(true);
+            $('body').animate({
                 scrollTop: elem.offset().top - offset
             }, 800);
         }
